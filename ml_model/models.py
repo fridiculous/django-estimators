3e26310cce2a9cf3c9f969558df14e4dba8022bf
Simon Frid <simon.frid@gmail.com>
2016-08-31 08:23:59 -0700

    Add ml_model app to persist and recieve ml_models

--- ml_model/models.py
+++ ml_model/models.py
@@ -2,16 +2,96 @@ from django.db import models
 import hashlib
 import datetime
 import os
+import dill
 
-
-def create_hash():
-    byte_string = os.urandom(64)
-    return hashlib.sha256(byte_string).hexdigest()
+from django.core.files.base import ContentFile
+from django.core.exceptions import ValidationError
 
 
 class MLModel(models.Model):
 
-    create_date = models.DateTimeField(auto_now_add=True, blank=False)
+    create_date = models.DateTimeField(auto_now_add=True, blank=False, null=False)
     description = models.CharField(max_length=256)
-    model_hash = models.CharField(max_length=64, default=create_hash, unique=True, blank=False)
-    s3_url = models.CharField(max_length=256, unique=True, blank=False)
+    model_hash = models.CharField(max_length=64, unique=True, default=None, null=False, editable=False)
+    model_file = models.FileField(upload_to='files/ml_models/', default=None, null=False, blank=True, editable=False)
+    _model = None
+
+    class Meta:
+        db_table = 'ml_model'
+
+    def __repr__(self):
+        return '<MLModel <Id %s> <Hash %s>: %s>' % (self.id, self.model_hash, self.model)
+
+    @property
+    def is_model_persisted(self):
+        return self.model_file.name is not None and os.path.isfile(self.model_file.path)
+
+    @classmethod
+    def _hash_of_model(cls, model):
+        data = dill.dumps(model)
+        return hashlib.md5(data).hexdigest()
+
+    @classmethod
+    def get_by_model(cls, model):
+        if model is not None:
+            model_hash = cls._hash_of_model(model)
+        return cls.get_by_model_hash(model_hash)
+
+    @classmethod
+    def get_by_model_hash(cls, model_hash):
+        query_set = cls.objects.filter(model_hash=model_hash)
+        return query_set.first()
+
+    @classmethod
+    def get_or_create(cls, model):
+        model_hash = cls._hash_of_model(model)
+        obj = cls.get_by_model_hash(model_hash)
+        if not obj:
+            # create object
+            obj = cls()
+            obj.model = model
+        return obj
+
+    @property
+    def model(self):
+        # return cached model into cache if the model is available in a file
+        if self._model is None:
+            self._load_model()
+        return self._model
+
+    @model.setter
+    def model(self, model):
+        model_hash = self._hash_of_model(model)
+        self._model = model
+        self.model_hash = model_hash
+        self.model_file.name = self.model_hash
+
+    def save(self, *args, **kwargs):
+        self.full_clean(exclude=['description'])
+        if not self.is_model_persisted:
+            self._persist_model()
+        super(MLModel, self).save(*args, **kwargs)
+
+    def clean(self):
+        if self.model_hash != self._hash_of_model(self.model):
+            raise ValidationError("model_hash '%s' should be set by the model '%s'" % (self.model_hash, self.model))
+        # if already persisted
+        obj = self.get_by_model(self.model_hash)
+        if self.id and self.model_hash != getattr(obj, 'model_hash', None):
+            raise ValidationError("Cannot persist updated model '%s'.  Create a new MLModel object." % self.model)
+
+    def _persist_model(self):
+        if self.model_hash:
+            data = dill.dumps(self.model)
+            cf = ContentFile(data)
+            self.model_file.save(self.model_hash, cf)
+            cf.close()
+            return True
+        return False
+
+    def _load_model(self):
+        # to force loading a model
+        if self.is_model_persisted:
+            self.model_file.open()
+            self._model = dill.loads(self.model_file.read())
+            self.model_file.close()
--- ml_model/tests.py
+++ ml_model/tests.py
@@ -1,3 +1,71 @@
 from django.test import TestCase
 
-# Create your tests here.
+from ml_model.models import MLModel
+import mock
+import os
+import tempfile
+
+from django.core.exceptions import ValidationError
+
+
+class MLModelCase(TestCase):
+
+    def setUp(self):
+        self.tmp_dir = tempfile.TemporaryDirectory()
+
+    def test_model_hash(self):
+        with self.settings(MEDIA_ROOT=self.tmp_dir.name):
+            m = MLModel(model=object, description='object test')
+            m.save()
+            self.assertEquals(m.model, object)
+            del m
+
+            n = MLModel.objects.get(description='object test')
+            # md5 hash of a "dill'ed" object = 'aa5d09692ebd6e2041f70555fcdac4b1'
+            self.assertEqual(n.model_hash, 'aa5d09692ebd6e2041f70555fcdac4b1')
+            # assert loaded after calling n.model
+            self.assertEquals(n.model, object)
+
+    def test_model_persistance(self):
+        with self.settings(MEDIA_ROOT=self.tmp_dir.name):
+            m = MLModel(model=object, description='another object')
+            self.assertEqual(os.path.exists(m.model_file.path), False)
+            self.assertEqual(m.is_model_persisted, False)
+
+            m.save()
+            self.assertEqual(os.path.exists(m.model_file.path), True)
+            self.assertEqual(m.is_model_persisted, True)
+
+    def test_get_or_create(self):
+        with self.settings(MEDIA_ROOT=self.tmp_dir.name):
+            m = MLModel.get_or_create('new_original_object')
+            m.save()
+            self.assertEquals(m.model, 'new_original_object')
+
+            n = MLModel.get_or_create('new_original_object')
+            self.assertEquals(m, n)
+
+    def test_update_model_fail(self):
+        with self.settings(MEDIA_ROOT=self.tmp_dir.name):
+            m = MLModel(model='uneditable_object')
+            m.model = 'object_edited_before_persistance'
+            m.save()
+            m.model = 'object_edited_after_persistance'
+            self.assertRaises(ValidationError, m.save)
+
+    def test_hashing_func(self):
+        model_hash = MLModel._hash_of_model('abcd')
+        self.assertEqual(model_hash, 'aa5ef22a3e1ec0c5f7f22288e08d7969')
+
+    def test_model_hash_diff_fail(self):
+        with self.settings(MEDIA_ROOT=self.tmp_dir.name):
+            m = MLModel()
+            m.model_hash = 'randomly set hash'
+            self.assertRaises(ValidationError, m.save)
+
+            m = MLModel(model='unique_object')
+            m.model_hash = 'randomly set hash'
+            self.assertRaises(ValidationError, m.save)
+
+    def tearDown(self):
+        self.tmp_dir.cleanup()
