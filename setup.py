818cce7731c1e4fb51df72de75bf323d5601d431
Simon Frid <simon.frid@gmail.com>
2016-09-23 13:15:17 -0700

    Add Testing Requirements File and update tox

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
-from setuptools import find_packages, setup
+
 from pip.req import parse_requirements
+from setuptools import find_packages, setup
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
