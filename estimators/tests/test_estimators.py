24749cc431e1a17927d736be1a41ba66191fa04e
Simon Frid <simon.frid@gmail.com>
2016-11-07 22:20:02 -0800

    Replace Django UnitTest with Pytest

--- estimators/tests/test_estimators.py
+++ estimators/tests/test_estimators.py
@@ -1,84 +1,76 @@
 import os
 import tempfile
 
-from django.core.exceptions import ValidationError
-from django.test import TestCase
 from estimators.models.estimators import Estimator
+from django.core.exceptions import ValidationError
+
+import pytest
 
 
-class EstimatorCase(TestCase):
-
-    def setUp(self):
-        self.tmp_dir = tempfile.TemporaryDirectory()
+@pytest.mark.django_db
+class TestEstimator():
 
     def test_object_hash(self):
-        with self.settings(MEDIA_ROOT=self.tmp_dir.name):
-            m = Estimator(estimator=object, description='object test')
-            m.save()
-            self.assertEquals(m.estimator, object)
-            del m
+        m = Estimator(estimator=object, description='object test')
+        m.save()
+        assert m.estimator == object
+        del m
 
-            n = Estimator.objects.get(description='object test')
-            # sklearn hash of a object = 'd9c9f286391652b89978a6961b52b674'
-            self.assertEqual(n.object_hash, 'd9c9f286391652b89978a6961b52b674')
-            # assert loaded after calling n.estimator
-            self.assertEquals(n.estimator, object)
-            self.assertEquals(Estimator._compute_hash(
-                object), 'd9c9f286391652b89978a6961b52b674')
+        n = Estimator.objects.get(description='object test')
+        # sklearn hash of a object = 'd9c9f286391652b89978a6961b52b674'
+        assert n.object_hash == 'd9c9f286391652b89978a6961b52b674'
+        # assert loaded after calling n.estimator
+        assert n.estimator == object
+        assert Estimator._compute_hash(object) == 'd9c9f286391652b89978a6961b52b674'
 
     def test_estimator_persistance(self):
-        with self.settings(MEDIA_ROOT=self.tmp_dir.name):
-            m = Estimator(estimator=object, description='another object')
-            self.assertEqual(os.path.exists(m.object_file.path), False)
-            self.assertEqual(m.is_persisted, False)
+        m = Estimator(estimator=object, description='another object')
+        assert os.path.exists(m.object_file.path) == False
+        assert m.is_persisted == False
 
-            m.save()
-            self.assertEqual(os.path.exists(m.object_file.path), True)
-            self.assertEqual(m.is_persisted, True)
+        m.save()
+        assert os.path.exists(m.object_file.path) == True
+        assert m.is_persisted == True
 
     def test_get_or_create(self):
-        with self.settings(MEDIA_ROOT=self.tmp_dir.name):
-            m = Estimator.get_or_create('new_original_object')
-            m.save()
-            self.assertEquals(m.estimator, 'new_original_object')
+        m = Estimator.get_or_create('new_original_object')
+        m.save()
+        assert m.estimator == 'new_original_object'
 
-            n = Estimator.get_or_create('new_original_object')
-            self.assertEquals(m, n)
+        n = Estimator.get_or_create('new_original_object')
+        assert m == n
 
     def test_create_from_file(self):
-        with self.settings(MEDIA_ROOT=self.tmp_dir.name):
-            obj = "{'key': 'value'}"
-            m = Estimator(estimator=obj)
-            m.save()
-            object_hash = m.object_hash
-            file_path = m.object_file.name
-            del m
+        obj = "{'key': 'value'}"
+        m = Estimator(estimator=obj)
+        m.save()
+        object_hash = m.object_hash
+        file_path = m.object_file.name
+        del m
 
-            m = Estimator.create_from_file(file_path)
-            self.assertEquals(m.estimator, obj)
-            self.assertEquals(m.object_hash, object_hash)
+        m = Estimator.create_from_file(file_path)
+        assert m.estimator == obj
+        assert m.object_hash == object_hash
 
     def test_update_estimator_fail(self):
-        with self.settings(MEDIA_ROOT=self.tmp_dir.name):
-            m = Estimator(estimator='uneditable_object')
-            m.estimator = 'object_edited_before_persistance'
+        m = Estimator(estimator='uneditable_object')
+        m.estimator = 'object_edited_before_persistance'
+        m.save()
+        m.estimator = 'object_edited_after_persistance'
+        with pytest.raises(ValidationError):
             m.save()
-            m.estimator = 'object_edited_after_persistance'
-            self.assertRaises(ValidationError, m.save)
 
     def test_hashing_func(self):
         object_hash = Estimator._compute_hash('abcd')
-        self.assertEqual(object_hash, '3062a9e3345c129799bd2c1603c2e966')
+        assert object_hash == '3062a9e3345c129799bd2c1603c2e966'
 
     def test_object_hash_diff_fail(self):
-        with self.settings(MEDIA_ROOT=self.tmp_dir.name):
-            m = Estimator()
-            m.object_hash = 'randomly set hash'
-            self.assertRaises(ValidationError, m.save)
+        m = Estimator()
+        m.object_hash = 'randomly set hash'
+        with pytest.raises(ValidationError):
+            m.save()
 
-            m = Estimator(estimator='unique_object')
-            m.object_hash = 'randomly set hash'
-            self.assertRaises(ValidationError, m.save)
-
-    def tearDown(self):
-        self.tmp_dir.cleanup()
+        m = Estimator(estimator='unique_object')
+        m.object_hash = 'randomly set hash'
+        with pytest.raises(ValidationError):
+            m.save()
--- estimators/tests/test_evaluations.py
+++ estimators/tests/test_evaluations.py
@@ -1,59 +1,51 @@
 import os
 import tempfile
+import pytest
 
-from django.test import TestCase
 
-
-class EvaluationCase(TestCase):
-
-    def setUp(self):
-        self.tmp_dir = tempfile.TemporaryDirectory()
+@pytest.mark.django_db
+class TestEvaluation():
 
     def test_evaluation_persistance(self):
-        with self.settings(MEDIA_ROOT=self.tmp_dir.name):
+        from sklearn.datasets import load_digits
+        from sklearn.ensemble import RandomForestClassifier
 
-            from sklearn.datasets import load_digits
-            from sklearn.ensemble import RandomForestClassifier
+        digits = load_digits()  # 1797 by 64
+        X = digits.data
+        y = digits.target
 
-            digits = load_digits()  # 1797 by 64
-            X = digits.data
-            y = digits.target
+        # simple splitting for validation testing
+        X_train, X_test = X[:1200], X[1200:]
+        y_train, y_test = y[:1200], y[1200:]
 
-            # simple splitting for validation testing
-            X_train, X_test = X[:1200], X[1200:]
-            y_train, y_test = y[:1200], y[1200:]
+        rfc = RandomForestClassifier()
+        rfc.fit(X_train, y_train)
 
-            rfc = RandomForestClassifier()
-            rfc.fit(X_train, y_train)
+        from estimators.models import Evaluator
 
-            from estimators.models import Evaluator
+        ev = Evaluator(X_test=X_test, y_test=y_test, estimator=rfc)
+        assert ev.X_test is X_test
+        assert ev.y_test is y_test
+        assert ev.estimator is rfc
+        assert ev.y_predicted is None
 
-            ev = Evaluator(X_test=X_test, y_test=y_test, estimator=rfc)
-            assert ev.X_test is X_test
-            assert ev.y_test is y_test
-            assert ev.estimator is rfc
-            assert ev.y_predicted is None
+        assert os.path.exists(
+            ev._estimator_proxy.object_file.path) is False
+        assert os.path.exists(ev._X_test_proxy.object_file.path) is False
+        assert os.path.exists(ev._y_test_proxy.object_file.path) is False
+        assert os.path.exists(
+            ev._y_predicted_proxy.object_file.path) is False
 
-            assert os.path.exists(
-                ev._estimator_proxy.object_file.path) is False
-            assert os.path.exists(ev._X_test_proxy.object_file.path) is False
-            assert os.path.exists(ev._y_test_proxy.object_file.path) is False
-            assert os.path.exists(
-                ev._y_predicted_proxy.object_file.path) is False
+        er = ev.evaluate()
 
-            er = ev.evaluate()
+        assert er.X_test is X_test
+        assert er.y_test is y_test
+        assert er.estimator is rfc
+        assert er.y_predicted is not None
+        assert len(er.y_predicted) == len(y_test)
 
-            assert er.X_test is X_test
-            assert er.y_test is y_test
-            assert er.estimator is rfc
-            assert er.y_predicted is not None
-            assert len(er.y_predicted) == len(y_test)
-
-            assert os.path.exists(er._estimator_proxy.object_file.path) is True
-            assert os.path.exists(er._X_test_proxy.object_file.path) is True
-            assert os.path.exists(er._y_test_proxy.object_file.path) is True
-            assert os.path.exists(
-                er._y_predicted_proxy.object_file.path) is True
-
-    def tearDown(self):
-        self.tmp_dir.cleanup()
+        assert os.path.exists(er._estimator_proxy.object_file.path) is True
+        assert os.path.exists(er._X_test_proxy.object_file.path) is True
+        assert os.path.exists(er._y_test_proxy.object_file.path) is True
+        assert os.path.exists(
+            er._y_predicted_proxy.object_file.path) is True
