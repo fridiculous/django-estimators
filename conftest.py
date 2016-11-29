c56f8047327647129ac2f484a1a10358e1b78006
Simon Frid <simon.frid@gmail.com>
2016-11-29 11:24:48 -0800

    Remove tmp directory after testing

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import django
 import os
+import shutil
 
 
 def pytest_report_header(config):
@@ -10,3 +11,9 @@ def pytest_configure(config):
     # using test version of settings.py
     os.environ['DJANGO_SETTINGS_MODULE'] = "estimators.tests.settings"
     django.setup()
+
+
+def pytest_unconfigure(config):
+    """Remove tmp directory for testings"""
+    from estimators.tests import settings
+    shutil.rmtree(settings.MEDIA_ROOT)
